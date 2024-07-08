cc666e3a20a61aab141f7ceaa710b5e75d50b3f7
Hagemaroo22 <162131935+Hagemaroo22@users.noreply.github.com>
2024-07-09 00:35:49 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,11 @@
+<div class="page-title">Simple Attendance Management System</div>
+<hr>
+<!DOCTYPE html> 
+<html lang='en'> 
+<head> 
+    <title>Test Page</title> 
+</head> 
+<body> 
+<center><img src='LOGO-GIF.gif' alt='photo of me'" width="500" height="500"/> </center> 
+</body> 
+</html> 
